dd776daf89551b0f15367a3c550e5a813aaeca38
Delaath <154479278+Delaath@users.noreply.github.com>
2024-10-31 22:25:55 +0000

    Deploying to gh-pages from @ Delaath/Simply-Chocolate@a02d19ef71beff21c83ed2d713d1ae92a7a6843a 🚀

--- index.7a282c2d.js
+++
@@ -1,2 +0,0 @@
-(()=>{const o={openModalBtn:document.querySelector("[data-open-modal]"),closeModalBtn:document.querySelector("[data-close-modal]"),modal:document.querySelector("[data-modal]")};function e(){o.modal.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
-//# sourceMappingURL=index.7a282c2d.js.map
